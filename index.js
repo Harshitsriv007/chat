d71f4449354972c8abf9bc1338befb96e396f3cf
Harshitsriv007 <Harshit.srivastv726@gmail.com>
2023-03-28 23:54:29 +0530

    new

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+// const express = require("https://unpkg.com/express@5.0.0-beta.1/index.js");
+// const bodyparser = require("https://cdn.jsdelivr.net/npm/body-parser@1.20.2/lib/types/raw.min.js");
 const bodyparser = require("body-parser");
 var path = require('path');
 
@@ -15,8 +17,11 @@ const server = app.listen(PORT, () => {
 });
 
 const io = require('socket.io')(server);
+// const io = require('https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.6.1/socket.io.js')(server);
 
 
+
+{/* <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.6.1/socket.io.js" integrity="sha512-xbQU0+iHqhVt7VIXi6vBJKPh3IQBF5B84sSHdjKiSccyX/1ZI7Vnkt2/8y8uruj63/DVmCxfUNohPNruthTEQA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> */}
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/public/index.html');
